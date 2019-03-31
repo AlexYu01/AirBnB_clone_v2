882105822dfa720040d70c70e9744a1c6d6e7e03
Paul Lave <pmlave@gmail.com>
2019-03-31 21:32:28 +0000

    Initial commit of both MySQL server setup scripts

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- This script sets up another MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'loclahost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
