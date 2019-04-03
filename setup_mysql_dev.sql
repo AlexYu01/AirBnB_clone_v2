6e6669e07bdf619fb28af65ba2781dccc0b48db9
AlexYu01 <tengalexyu@gmail.com>
2019-04-03 11:53:32 -0700

    Switch to new DB

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,11 @@
 -- This script sets up the MySQL server for the project
 -- Create the DB if it doesnt exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Switch to new database
+USE hbnb_dev_db;
 -- Create a user if it doesnt exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'loclahost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant privileges to the newly created user to the db
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 -- Grant SELECT prvilige to the newly created user to a schema
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
