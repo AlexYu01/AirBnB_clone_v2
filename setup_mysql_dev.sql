0e0702afa9575bae01600d7b5a62492a1a8662af
AlexYu01 <tengalexyu@gmail.com>
2019-04-03 11:57:02 -0700

    Remove `

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Switch to new database
 USE hbnb_dev_db;
 -- Create a user if it doesnt exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'loclahost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant privileges to the newly created user to the db
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant SELECT prvilige to the newly created user to a schema
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
