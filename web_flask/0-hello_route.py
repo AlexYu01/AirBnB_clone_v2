f30faaba926853c1a9eab6d3197a86c4bfe223c0
AlexYu01 <tengalexyu@gmail.com>
2019-04-24 18:01:56 -0700

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
     Starts a Flash web application
-'''
+"""
 from flask import Flask
 
 
@@ -10,9 +10,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    '''
+    """
         Prints `Hello HBNB!` when accessing root of website
-    '''
+    """
     return 'Hello HBNB!'
 
 app.run(host='0.0.0.0')
