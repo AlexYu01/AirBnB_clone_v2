d1f79e99730db25b0c75c2e908d8fdff08a2144e
AlexYu01 <tengalexyu@gmail.com>
2019-04-25 01:19:34 +0000

    Guard name

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,16 +3,16 @@
     Starts a Flash web application
 """
 from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-    """
-        Prints `Hello HBNB!` when accessing root of website
-    """
-    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def hello():
+        """
+            Prints `Hello HBNB!` when accessing root of website
+        """
+        return 'Hello HBNB!'
+
     app.run(host='0.0.0.0')
