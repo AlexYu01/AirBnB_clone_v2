5a4a6f485ee840b01073f5982db966e3245d7745
AlexYu01 <tengalexyu@gmail.com>
2019-05-31 04:12:01 -0700

    Remove check for if __name__ is __main__

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,15 +4,13 @@
 """
 from flask import Flask
 
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app = Flask(__name__)
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    """
+        Prints `Hello HBNB!` when accessing root of website
+    """
+    return 'Hello HBNB!'
 
-    @app.route('/airbnb-onepage/', strict_slashes=False)
-    def hello():
-        """
-            Prints `Hello HBNB!` when accessing root of website
-        """
-        return 'Hello HBNB!'
-
-    app.run(host='0.0.0.0', port='5000')
+app.run(host='0.0.0.0', port='5000')
